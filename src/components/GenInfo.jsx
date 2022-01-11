0d853401a651094b4af714ee5c260bd7509c6164
bananabread08 <rmkfermin@gmail.com>
2022-01-11 11:33:02 +0800

    added labels for input plus basic styling

--- src/components/GenInfo.jsx
+++ src/components/GenInfo.jsx
@@ -12,12 +12,21 @@ const StyledInput = styled.input`
   padding: 0.5rem;
   border-radius: 5px;
   border: 1px solid transparent;
-  width: 40%;
   &:focus {
-    border: 2px solid black;
+    border: 2px solid #7167c7;
     margin: -1px;
   }
+  width: 40vw;
 `;
+const InputContainer = styled.div`
+  width: 60vw;
+
+  label {
+    display: inline-block;
+    width: 150px;
+  }
+`;
+
 export default function GenInfo() {
   const [GenInfoValues, setGenInfoValues] = useState({
     name: 'Phoenix Wright',
@@ -46,43 +55,64 @@ export default function GenInfo() {
   return (
     <section>
       <StyledForm onSubmit={handleSubmit}>
-        <StyledInput
-          type="text"
-          placeholder="Name.."
-          name="name"
-          onChange={handleChange}
-        ></StyledInput>
-        <StyledInput
-          type="text"
-          placeholder="Role.."
-          name="role"
-          onChange={handleChange}
-        ></StyledInput>
-        <StyledInput
-          type="number"
-          placeholder="Contact Number.."
-          name="contact"
-          onChange={handleChange}
-        ></StyledInput>
-        <StyledInput
-          type="text"
-          placeholder="Address.."
-          name="address"
-          onChange={handleChange}
-        ></StyledInput>
-        <StyledInput
-          type="email"
-          placeholder="Email Address.."
-          name="email"
-          onChange={handleChange}
-        ></StyledInput>
-        <StyledInput
-          type="text"
-          placeholder="Website (if any).."
-          name="site"
-          onChange={handleChange}
-        ></StyledInput>
-        <input className="Submit-btn" type="submit" value="Submit" />
+        <InputContainer>
+          <label>Name:</label>
+          <StyledInput
+            type="text"
+            placeholder="Name.."
+            name="name"
+            onChange={handleChange}
+          ></StyledInput>
+        </InputContainer>
+        <InputContainer>
+          <label>Role:</label>
+          <StyledInput
+            type="text"
+            placeholder="Role.."
+            name="role"
+            onChange={handleChange}
+          ></StyledInput>
+        </InputContainer>
+        <InputContainer>
+          <label>Contact Number:</label>
+          <StyledInput
+            type="number"
+            placeholder="Contact Number.."
+            name="contact"
+            onChange={handleChange}
+          ></StyledInput>
+        </InputContainer>
+        <InputContainer>
+          <label>Address:</label>
+          <StyledInput
+            type="text"
+            placeholder="Address.."
+            name="address"
+            onChange={handleChange}
+          ></StyledInput>
+        </InputContainer>
+        <InputContainer>
+          <label>Email Address:</label>
+          <StyledInput
+            type="email"
+            placeholder="Email Address.."
+            name="email"
+            onChange={handleChange}
+          ></StyledInput>
+        </InputContainer>
+        <InputContainer>
+          <label>Website:</label>
+          <StyledInput
+            type="text"
+            placeholder="Website (if any).."
+            name="site"
+            onChange={handleChange}
+          ></StyledInput>
+        </InputContainer>
+
+        <button type="submit" value="Submit">
+          Submit
+        </button>
       </StyledForm>
     </section>
   );
