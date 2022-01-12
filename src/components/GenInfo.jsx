cbd3701b2d8de6c56c8c9a7120b9daa894e22951
bananabread08 <rmkfermin@gmail.com>
2022-01-12 15:03:17 +0800

    fixed centering of form, styled button and section

--- src/components/GenInfo.jsx
+++ src/components/GenInfo.jsx
@@ -1,12 +1,40 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const StyledSection = styled.section`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 10px;
+  padding: 10px;
+  font-size: 14px;
+  border: 1px solid white;
+  width: 90vw;
+`;
+
+const SubmitBtn = styled.button`
+  cursor: pointer;
+  background-color: #1976d2;
+  color: #fff;
+  padding: 4px 16px;
+  min-width: 64px;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  text-transform: uppercase;
+  border-radius: 15px 15px;
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
+    0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+  &:hover {
+    background-color: #2268ad;
+  }
 `;
 const StyledInput = styled.input`
   padding: 0.5rem;
@@ -14,16 +42,32 @@ const StyledInput = styled.input`
   border: 1px solid transparent;
   &:focus {
     border: 2px solid #7167c7;
-    margin: -1px;
+    margin: -1px 0 -1px 0; // so element sibling won't move due to margin displacement
   }
-  width: 40vw;
+  width: 50vw;
+`;
+
+const StyledTextArea = styled.textarea`
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  resize: none;
+  &:focus {
+    border: 2px solid #7167c7;
+    margin: -1px 0 -1px 0;
+  }
+  width: 50vw;
 `;
 const InputContainer = styled.div`
-  width: 60vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   label {
     display: inline-block;
     width: 150px;
+    text-align: end;
+    padding-right: 10px;
   }
 `;
 
@@ -53,14 +97,15 @@ export default function GenInfo() {
   };
 
   return (
-    <section>
+    <StyledSection>
       <StyledForm onSubmit={handleSubmit}>
         <InputContainer>
           <label>Name:</label>
           <StyledInput
             type="text"
-            placeholder="Name.."
+            placeholder="Juan E. Dela Cruz"
             name="name"
+            value={GenInfoValues.name}
             onChange={handleChange}
           ></StyledInput>
         </InputContainer>
@@ -68,52 +113,58 @@ export default function GenInfo() {
           <label>Role:</label>
           <StyledInput
             type="text"
-            placeholder="Role.."
+            placeholder="Software Engineer"
             name="role"
+            value={GenInfoValues.role}
             onChange={handleChange}
           ></StyledInput>
         </InputContainer>
         <InputContainer>
-          <label>Contact Number:</label>
+          <label>Contact #:</label>
           <StyledInput
             type="number"
-            placeholder="Contact Number.."
+            placeholder="+6391612345678"
             name="contact"
+            value={GenInfoValues.contact}
             onChange={handleChange}
           ></StyledInput>
         </InputContainer>
         <InputContainer>
           <label>Address:</label>
-          <StyledInput
+          <StyledTextArea
             type="text"
-            placeholder="Address.."
+            placeholder="221B Baker Street, London, England"
             name="address"
+            value={GenInfoValues.address}
+            Rows="2"
             onChange={handleChange}
-          ></StyledInput>
+          ></StyledTextArea>
         </InputContainer>
         <InputContainer>
           <label>Email Address:</label>
           <StyledInput
             type="email"
-            placeholder="Email Address.."
+            placeholder="jedelacruz1@gmail.com"
             name="email"
+            value={GenInfoValues.email}
             onChange={handleChange}
           ></StyledInput>
         </InputContainer>
         <InputContainer>
-          <label>Website:</label>
+          <label>Website (if any):</label>
           <StyledInput
             type="text"
-            placeholder="Website (if any).."
+            placeholder="github.com/jedelacruz1"
             name="site"
+            value={GenInfoValues.site}
             onChange={handleChange}
           ></StyledInput>
         </InputContainer>
 
-        <button type="submit" value="Submit">
+        <SubmitBtn type="submit" value="Submit">
           Submit
-        </button>
+        </SubmitBtn>
       </StyledForm>
-    </section>
+    </StyledSection>
   );
 }
