d22974f65a8b451ffd8512447c412d91afdeb487
bananabread08 <rmkfermin@gmail.com>
2022-01-21 13:56:28 +0800

    implemented AddEducation function to add a new Education component

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,15 +1,31 @@
+import React, { useState } from 'react';
 import GenInfo from './GenInfo';
 import Education from './Education';
 import Experience from './Experience';
 import Skills from './Skills';
+import uniqid from 'uniqid';
+import { SubmitBtn } from './styles/shared/sharedStyle';
 
 export default function Edit() {
+  const [schoolList, setSchoolList] = useState([]);
+
+  const addEducation = (e) => {
+    let id = uniqid();
+    e.preventDefault();
+    setSchoolList((prevState) =>
+      prevState.concat({ school: <Education key={id} />, id: id })
+    );
+  };
+
   return (
     <>
       <h2>General Information</h2>
       <GenInfo />
       <h2>Education </h2>
-      <Education />
+      {schoolList.map((comp) => (
+        <Education key={comp.id} id={comp.id} />
+      ))}
+      <SubmitBtn onClick={addEducation}>Add More</SubmitBtn>
       <h2>Experience</h2>
       <Experience />
       <h2>Skills</h2>
